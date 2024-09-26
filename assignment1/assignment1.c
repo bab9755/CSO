20992e8cd058895b0f2adb9c8e2d7052f503cb37
Boubacar Ballo <bab9755@nyu.edu>
2024-09-25 22:49:06 -0400

    funish read_recordf

--- assignment1/assignment1.c
+++ assignment1/assignment1.c
@@ -65,11 +65,45 @@ int compare_salary(PERSONNEL_REC* p1, PERSONNEL_REC* p2)
 
 PERSONNEL_REC* new_record(char* first_name, char* last_name, char middle_initial, int age, int salary, long id_num){
     PERSONNEL_REC* record = malloc(sizeof(PERSONNEL_REC));
+    record->first_name = malloc((strlen(first_name) + 1) * sizeof(char)); // Allocate memory for first_name
+    record->last_name = malloc((strlen(last_name) + 1) * sizeof(char));   // Allocate memory for last_name
+    strcpy(record->first_name, first_name); // Copy first_name into allocated memory
+    strcpy(record->last_name, last_name);   // Copy last_name into allocated memory
+    record->age = age;
+    record->salary = salary;
+    record->middle_initial = middle_initial;
+    record->id_num = id_num;
+
+    return record;
     
+
+}
+
+
+PERSONNEL_REC* read_record(){
+    char* last_name = malloc(100*sizeof(char));
+    char* first_name = malloc(100*sizeof(char));
+    char middle_initial;
+    int age, salary;
+    long id_num;
+
+    printf("Enter the records in the following order: (first_name last_name, middle_initial age salary id_num): \n");
+    
+    while (1){
+        if(scanf("%99s %99s %c %d %d %ld", last_name, first_name, &middle_initial, &age, &salary, &id_num) != 6){
+            break;
+        }
+    }
+    PERSONNEL_REC* record = malloc(sizeof(PERSONNEL_REC));
+    record = new_record(first_name, last_name, middle_initial, age, salary, id_num);
+
+    printf("You created a new record with first name %99s and id number %ld: ", first_name, id_num);
+
+    return record;
 }
 
 
 int main(){
-
+    read_record();
     return 0;
 }
