ea737697cebacfd238bdc887eb5e16fe057bfec9
Boubacar Ballo <bab9755@nyu.edu>
2024-10-15 16:03:13 -0400

    file config

---
+++ assignment2/exponent.s
@@ -0,0 +1,42 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.globl	_exponent
+	.p2align	4, 0x90
+
+	# This function takes three parameters:
+	#   -- a 32-bit integer x in the %edi register
+	#   -- a 32-bit integer y in the %esi register	
+	#   -- a 32-bit integer n in the %edx register
+	# It returns x^n + y^n, that is, x to the nth power plus y to the nth power.
+	# The return value, being 32 bits, must be placed in the %eax register.
+	
+_exponent:	
+
+	# Note: You can overwrite the 64-bit registers %rax, %rcx, %rdx, %rsi, %rdi, %r8,
+	# %r9, %r10, %r11 (as well as their 32-bit halves, %eax, %ecx,
+	# %edx, %esi, %edi, %r8d, %r9d, %r10d, %r11d) as you like. These are
+	# "caller-saved" registers.
+	
+	pushq	%rbp	     # LEAVE THIS ALONE
+	movq	%rsp, %rbp   # LEAVE THIS ALONE
+
+	/* FILL THIS IN */   # use the 32-bit register %eax to hold x^n, initially 1
+	                     # 
+	
+        /* FILL THIS IN */   # use a 32-bit register to hold y^n, initially 1
+	/* FILL THIS IN	*/   # use a 32-bit register to hold i, initially 0
+LOOP_TOP:
+        /* FILL THIS IN	*/   # compare i to n (remember, the comparison appears reversed)
+        /* FILL THIS IN	*/   # and if i is greater or equal to n, jump to DONE
+
+        /* FILL THIS IN	*/   # multiply register holding x^n by x
+        /* FILL THIS IN	*/   # multiply register holding y^n by y 
+        /* FILL THIS IN	*/   # i++
+        /* FILL THIS IN	*/   # jump to LOOP_TOP
+DONE:
+        /* FILL THIS IN	*/   # add y^n to x^n
+        		     # Since the result is already in %eax, just leave it there
+
+	popq	%rbp	     # LEAVE THIS ALONE
+	retq                 # LEAVE THIS ALONE
+
+	
---
+++ assignment2/functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // to use printf and scanf
+#include <stdlib.h> // to use malloc
+#include <string.h> // to use strlen, strcmp, and strcpy
+
+
+void print_bits(int * x){
+    
+}
+
+int main(){
+    return 0;
+}
